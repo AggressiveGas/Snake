051b4d24cc57cc4e5cfea0f317b81b7d562262dd
Brandon Symansiv <brandonsymansiv@csus.edu>
2024-03-22 20:48:04 -0700

    Made GameObject class abstract and added abstract method draw.

--- app/src/main/java/com/example/snake/GameObject.java
+++ app/src/main/java/com/example/snake/GameObject.java
@@ -1,13 +1,17 @@
 package com.example.snake;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Point;
 
-public class GameObject {
+public abstract class GameObject {
     private Point location;
 
     public GameObject(int x, int y) {
         this.location = new Point(x, y);
     }
+
+    public abstract void draw(Canvas canvas, Paint paint);
 
     public Point getLocation() {
         return location;
